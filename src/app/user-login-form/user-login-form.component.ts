532ca5476591763debb95d8ea39f8d049fc0d369
DesJCodes <141986568+DesJCodes@users.noreply.github.com>
2024-09-06 15:31:54 -0400

    "Updated user login form component HTML and TS files to add Material Design elements and styles"

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -8,6 +8,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-user-login-form',
@@ -20,6 +21,7 @@ import { MatCardModule } from '@angular/material/card';
     MatInputModule,
     MatButtonModule,
     MatCardModule,
+    MatIconModule,
   ],
 })
 export class UserLoginFormComponent implements OnInit {

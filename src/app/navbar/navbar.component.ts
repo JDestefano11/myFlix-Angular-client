40ca9b1a54d3941948829589c234fba35f11295a
DesJCodes <141986568+DesJCodes@users.noreply.github.com>
2024-09-07 14:14:17 -0400

    "Added NavbarComponent to app.component.html and imports in app.component.ts"

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { NgIf } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, NgIf, RouterLink],
+})
+export class NavbarComponent implements OnInit {
+  isLoggedIn: boolean = false;
+  isWelcomePage: boolean = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isWelcomePage = event.url === '/welcome';
+        this.isLoggedIn = !!localStorage.getItem('token');
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.isLoggedIn = !!localStorage.getItem('token');
+  }
+
+  logout(): void {
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
+    this.isLoggedIn = false;
+    this.router.navigate(['/welcome']);
+  }
+}
